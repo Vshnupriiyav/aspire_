f3aa24b6ef25fba7e07e28fb13af73dd5c5c52c8
vishnupriya <vishnupriya.vishv@gmail.com>
2025-04-22 11:46:37 +0530

    initial commit

---
+++ connector.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import pymysql
+import sys
+import sqlalchemy
+
+
+
+cred = { 'user': 'vishnupriya', 'password': 'yFl#Cy3qzWW7f#OrxW4IB!P'}
+
+def data_fetch_v2(query,*, 
+                  cred=cred, db='aspiredb', 
+                  host="13.235.168.161", **kwargs):
+    """
+    Fetches data for the given query from the database.
+
+    Args:
+        query (str):
+        cred (dict): {"user" : "<user name>", "password" : "<password>"}
+        db (str): 
+        host (str):
+        kwargs : 
+
+    Returns:
+        success_flag (bool):
+        output (pd.DataFrame): 
+        error_msg (str):
+    """
+    success_flag = False
+    output = pd.DataFrame()
+    error_msg = ""
+    db_url = rf"mysql+pymysql://{cred['user']}:{cred['password']}@{host}/{db}"
+    engine = sqlalchemy.create_engine(db_url)
+    output = pd.read_sql(query, engine, **kwargs)
+    print(output)
+    success_flag = True
+
+    try:
+        db_url = rf"mysql+pymysql://{cred['user']}:{cred['password']}@{host}/{db}"
+        engine = sqlalchemy.create_engine(db_url)
+        output = pd.read_sql(query, engine, **kwargs)
+        print(output)
+        success_flag = True
+    except Exception as e:
+        success_flag = False
+        error_msg = f"error in data_fetch, {type(e)._name_}: {str(e)}"
+    finally:
+        return success_flag, output, error_msg
+    
+ 
